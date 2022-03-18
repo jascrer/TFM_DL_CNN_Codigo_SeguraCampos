aafd04d05363d8f1d4facc42f4b6fea965ce2c6b
Jose Arnoldo Segura <jarnoldo2809@gmail.com>
2022-03-17 19:39:09 -0600

    Initial commit of the environment

---
+++ tfm_dl-cnn_codigo_seguracampos/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Este proyecto corresponde al Trabajo Final de Master de la maestria de Inteligencia Artificial y Big Data del alumno Jose Arnoldo Segura Campos',
+    author='Jose Arnoldo Segura Campos',
+    license='BSD-3',
+)
