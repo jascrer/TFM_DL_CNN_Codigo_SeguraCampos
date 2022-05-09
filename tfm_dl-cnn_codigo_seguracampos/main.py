58386d21d00704c13d223a6da368f1315dbc7faf
Jose Arnoldo Segura <jarnoldo2809@gmail.com>
2022-05-08 20:54:38 -0600

    Adding the latest changes

--- tfm_dl-cnn_codigo_seguracampos/main.py
+++ tfm_dl-cnn_codigo_seguracampos/main.py
@@ -5,7 +5,7 @@ Experiment will be executed in this file
 """
 
 import warnings
-# import torch
+import torch
 import src.experiment_definitions as experiments
 
 warnings.filterwarnings('ignore')
@@ -14,28 +14,29 @@ PHI_BATCH_SIZE = 2500
 ALEX_BATCH_SIZE = 250
 PAPER_LR = 0.01
 GDEXPLOIT_LR = 0.001
+SMALLEST_LR = 1e-4
 EPOCH_COUNT = 150
 SEED = 42
 
 def main():
     """Main function. Point of execution"""
     # Execution of the experiment for PHI Model with no augmentation transformations
-    experiments.phi_model_no_transform(EPOCH_COUNT, PHI_BATCH_SIZE, SEED, PAPER_LR)
+    #experiments.phi_model_no_transform(EPOCH_COUNT, PHI_BATCH_SIZE, SEED, PAPER_LR)
 
     # Execution of the experiment for PHI Model with flip augmentation transformations
-    experiments.phi_model_flip_transform(EPOCH_COUNT, PHI_BATCH_SIZE, SEED, PAPER_LR)
+    #experiments.phi_model_flip_transform(EPOCH_COUNT, PHI_BATCH_SIZE, SEED, PAPER_LR)
 
     # Empty CUDA memory
-    #torch.cuda.empty_cache()
+    torch.cuda.empty_cache()
 
     # Execution of the experiment for AlexNet Model with no augmentation transformations
-    #experiments.alexnet_model_no_transform(EPOCH_COUNT, ALEX_BATCH_SIZE, SEED, GDEXPLOIT_LR)
+    #experiments.alexnet_model_no_transform(EPOCH_COUNT, ALEX_BATCH_SIZE, SEED, SMALLEST_LR)
 
     # Empty CUDA memory
     #torch.cuda.empty_cache()
 
     # Execution of the experiment for AlexNet Model with flip augmentation transformations
-    # experiments.alexnet_model_flip_transform(EPOCH_COUNT, ALEX_BATCH_SIZE, SEED, GDEXPLOIT_LR)
+    experiments.alexnet_model_flip_transform(EPOCH_COUNT, ALEX_BATCH_SIZE, SEED, SMALLEST_LR)
 
 if __name__ == "__main__":
     main()
